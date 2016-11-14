3be050f406bf0e260917c698a9401c8d7fe71298
jxc14 <John.Chynoweth-contractor@nreca.coop>
2016-11-14 09:16:17 -0500

    save kata page

--- src/service/kata-service.js
+++ src/service/kata-service.js
@@ -8,34 +8,40 @@ export class KataService {
         this.kataKey = "chynotestapp/kata/3";
         this.gunKey = location.origin + '/gun';
         this.gun = new Gun();
-        this.gkatas = this.gun.put(this.katas).key(this.kataKey);
-        this.setData(); 
+       this.gkatas = this.gun.put(this.katas).key(this.kataKey);
+        this.setData();
     }
 
 
     getKatas() {
-      return this.gkatas.get(this.kataKey).val();
-     // return [];
+     // return this.gkatas.get(this.kataKey).path("tests").val();
+     return [];
     }
 
 
     setData() {
 
-        let katas = [];
+        let kata;
+
         //Make sure add dataq only once
         if (this.gkatas.get(this.kataKey).val())
         {
             return;
         }
 
-        katas.push({
+          katas = {
+            id: 1,
+            tests : []
+        };
+
+        kata.tests.push({
             name: "first Javascript kata ",
             description: "Sampple Java script. The strings were taken out and need to be placed back into the application",
             code: "function findSequence(goal) { \n function find(start, history) { \n if (start == goal) \n return history; \n else if (start > goal) \n return null; \n else \n return find(start + 5, ( + history +  + 5)) || \n find(start * 3, ( + history +  * 3)); \n } \n  return find(1, 1); \n } \n ",
             assertion: 'Assert(true == true);'
         });
 
-        katas.push({
+        katas.tests.push({
             name: "second",
             description: "Just write to console. This is the most basic example you need to write the code from scratch",
             code: "console.writeline('2')",
@@ -43,8 +49,8 @@ export class KataService {
         });
 
         this.gkatas.put(katas).key(this.kataKey);
-        
+
     }
 
-    
+
 }
