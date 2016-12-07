af84db2c2c8285a2de3f581443672b5670960baa
jxc14 <John.Chynoweth-contractor@nreca.coop>
2016-12-07 14:57:13 -0500

    Add Node Code

---
+++ server/api.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var express = require('express');
+var router = express.Router();
+//var http = require('requestify');
+
+
+
+ //For testing
+router.get('/api/helloWorld', function (req, res)
+{
+    res.send('Hell World!!');
+});
+
+
+module.exports = router;
